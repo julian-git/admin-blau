578136e4450577124bc218dc4749c4a942551416
julian-git <julian-git@gmx.de>
2014-02-11 13:53:17 +0100

    added table socis

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
-			'username'  => 'root',
-			'password'  => '',
+			'database'  => 'admin_blau',
+			'username'  => 'admin_blau',
+			'password'  => 'admin_blau',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
---
+++ app/database/migrations/2014_02_11_124400_create_socis.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocis extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+	    Schema::create('socis', function($table) {
+		    $table->increments('id');
+		    $table->string('cognom1', 50);
+		    $table->string('cognom2', 50);
+		    $table->string('nom', 50);
+		    $table->string('mot', 50);
+		    $table->date('naixement');
+		    $table->string('dni', 15);
+		    $table->string('email', 50);
+		    $table->string('direccio', 200);
+		    $table->string('cp', 8);
+		    $table->string('poblacio', 50);
+		    $table->string('provincia', 30);
+		    $table->string('telefon', 12);
+		    $table->string('mobil', 12);
+		    $table->date('alta');
+		    $table->string('sexe', 1);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+	    Schema::drop('socis');
+	}
+
+}
