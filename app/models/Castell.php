a5d789ca4d56ee08a595c24b43b3868a52c1587d
julian-git <julian-git@gmx.de>
2014-02-13 10:59:46 +0100

    added gpl v3 to all files written here

--- app/models/Castell.php
+++ app/models/Castell.php
@@ -1,4 +1,20 @@
 <?php
+/*
+    (c) 2014 Castellers de la Vila de Gràcia
+    info@cvg.cat
+
+    This file is part of l'Admin Blau.
+
+    L'Admin Blau is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    L'Admin Blau is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
 
 class Castell extends Eloquent
 {
