8067768b538c0883f1628f87dd09a1369c120528
julian-git <julian-git@gmx.de>
2014-02-13 01:12:24 +0100

    initial commit

---
+++ app/database/migrations/2014_02_13_001137_pinyes.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class Pinyes extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
