781b8eb28d20c1fc6731942c33398899ca65fbe8
julian-git <julian-git@gmx.de>
2014-02-13 01:46:54 +0100

    database tables for pinyes

--- app/database/migrations/2014_02_13_001137_pinyes.php
+++ app/database/migrations/2014_02_13_001137_pinyes.php
@@ -11,12 +11,35 @@ class Pinyes extends Migration {
 	 */
 	public function up()
 	{
-	    Schema::create('castell', function($table) {
+	    Schema::create('castell_tipus', function($table) {
 		    $table->increments('id');
-		    $table->string('tipus', 10);
+		    $table->string('nom', 10);
+		});
+	    
+	    Schema::create('castells', function($table) {
+		    $table->increments('id');
+		    $table->integer('tipus_fk')->unsigned();
+		    $table->foreign('tipus_fk')->references('id')->on('castell_tipus');
 		    $table->date('data');
 		    $table->string('lloc', 20);
 		    $table->smallInteger('ordre');
+		});
+
+	    Schema::create('posicio', function($table) {
+		    $table->increments('id');
+		    $table->integer('castell_tipus_fk')->unsigned();
+		    $table->foreign('castell_tipus_fk')->references('id')->on('castell_tipus');
+		    $table->string('nom', 15);
+		});
+
+	    Schema::create('pinya', function($table) {
+		    $table->increments('id');
+		    $table->integer('casteller_id_fk')->unsigned();
+		    $table->foreign('casteller_id_fk')->references('id')->on('castellers');
+		    $table->integer('castell_id_fk')->unsigned();
+		    $table->foreign('castell_id_fk')->references('id')->on('castells');
+		    $table->integer('posicio_id_fk')->unsigned();
+		    $table->foreign('posicio_id_fk')->references('id')->on('posicio');
 		});
 	}
 
@@ -27,7 +50,10 @@ class Pinyes extends Migration {
 	 */
 	public function down()
 	{
-		//
+	    Schema::drop('pinya');
+	    Schema::drop('posicio');
+	    Schema::drop('castells');
+	    Schema::drop('castell_tipus');
 	}
 
 }
